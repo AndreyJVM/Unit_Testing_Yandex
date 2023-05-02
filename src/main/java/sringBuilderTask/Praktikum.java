2fc20cef2788085d214a1ff1e9be994bc665316a
Andrey Vorobev <mamin.xaker@yandex.ru>
2023-05-02 23:03:56 +0300

    Add new package sringBuilderTask and class sringBuilderTask/Practikum

---
+++ src/main/java/sringBuilderTask/Praktikum.java
@@ -0,0 +1,15 @@
+package sringBuilderTask;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        //Создай экземпляр StringBuilder, а затем вызови метод append() так, чтобы на экране появилась строка "Hello, java!".
+        // Метод append() нужно вызвать один раз.
+        String str = "Hello,";
+        StringBuilder sb = new StringBuilder(str); // создай экземпляр StringBuilder со строкой str
+
+        // Под этим комментарием должен быть вызов sb.append(..)
+        sb.append(" java!");
+        String asString = sb.toString();
+        System.out.println(asString);
+    }
+}
